e7e71c1a63f260b5c407e2fd0dd4c1fc93246271
pluswebsite <98026305+pluswebsite@users.noreply.github.com>
2023-03-11 16:38:05 +0100

    Update loginwith.js

--- packages/l/loginwith.js
+++
@@ -1,9 +0,0 @@
-class loginWith{
-  constructor(key){
-    this.key = key;
-  }
-  request(){
-    var url = "https://pluswebsite.github.io/loginwith/?&key=" + key;
-    window.open(url, '_blank').focus();
-  }
-}
